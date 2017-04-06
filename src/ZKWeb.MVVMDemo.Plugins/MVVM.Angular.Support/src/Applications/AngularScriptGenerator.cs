b0775adb4fccfad26e227a5bfb8b42919cc29e45
dudes-come <dudes@qq.com>
2017-04-06 12:19:58 +0800

    更新csproj

---
+++ src/ZKWeb.MVVMDemo.Plugins/MVVM.Angular.Support/src/Applications/AngularScriptGenerator.cs
@@ -0,0 +1,17 @@
+﻿using ZKWeb.Web;
+using ZKWebStandard.Ioc;
+
+namespace ZKWeb.MVVMDemo.Plugins.MVVM.Angular.Support.src.Applications {
+	/// <summary>
+	/// 负责处理AngularJS的服务脚本
+	/// </summary>
+	[ExportMany]
+	public class AngularScriptGenerator : IWebsiteStartHandler {
+		/// <summary>
+		/// 网站启动时生成脚本
+		/// </summary>
+		public void OnWebsiteStart() {
+			// TODO
+		}
+	}
+}
