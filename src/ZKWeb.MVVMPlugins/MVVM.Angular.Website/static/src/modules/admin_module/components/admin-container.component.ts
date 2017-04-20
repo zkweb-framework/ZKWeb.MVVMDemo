1e4d4a1f4d7e70d7eac149dc2434bf070d697dd4
dudes-come <dudes@qq.com>
2017-04-20 10:29:58 +0800

    修改后台样式

--- src/ZKWeb.MVVMPlugins/MVVM.Angular.Website/static/src/modules/admin_module/components/admin-container.component.ts
+++ src/ZKWeb.MVVMPlugins/MVVM.Angular.Website/static/src/modules/admin_module/components/admin-container.component.ts
@@ -5,4 +5,13 @@
 	templateUrl: '../views/admin-container.html',
 	styleUrls: [ '../styles/admin-container.scss' ]
 })
-export class AdminContainerComponent { }
+export class AdminContainerComponent {
+	activeMenuId: string;
+	themesVisible: boolean = false;
+	mobileMenuActive: boolean = false;
+
+	toggleMenu(e) {
+		this.mobileMenuActive = !this.mobileMenuActive;
+		e.preventDefault();
+	}
+}
--- src/ZKWeb.MVVMPlugins/MVVM.Angular.Website/static/src/vendor.ts
+++ src/ZKWeb.MVVMPlugins/MVVM.Angular.Website/static/src/vendor.ts
@@ -15,4 +15,5 @@ import 'rxjs/add/operator/mergeMap';
 // Styles
 import './vendor/styles/primeng/primeng.min.css';
 import './vendor/styles/primeng/theme.css';
+import './vendor/styles/primeng/site.css';
 import './vendor/styles/font-awesome/font-awesome.css';
