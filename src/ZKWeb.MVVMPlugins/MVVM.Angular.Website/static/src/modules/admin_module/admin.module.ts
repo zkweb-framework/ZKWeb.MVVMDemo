53ff2e2ca7cceac51159343b10b3d72242420cda
RandyBoy <75268967@qq.com>
2017-05-05 16:13:37 +0800

    修正路由

--- src/ZKWeb.MVVMPlugins/MVVM.Angular.Website/static/src/modules/admin_module/admin.module.ts
+++ src/ZKWeb.MVVMPlugins/MVVM.Angular.Website/static/src/modules/admin_module/admin.module.ts
@@ -26,30 +26,59 @@ import { AdminIndexComponent } from './components/admin-index.component';
 import { AdminLoginComponent } from './components/admin-login.component';
 
 const routes: Routes = [
-	{
-		path: '',
-		component: AdminIndexComponent,
-		pathMatch: 'full',
-		canActivate: [AuthGuard],
-		data: { auth: { requireUserType: UserTypes.ICanUseAdminPanel } }
-	},
-	{ path: 'login', component: AdminLoginComponent },
-	{
-		path: 'about_website', component: AdminAboutWebsiteComponent,
-		canActivate: [AuthGuard],
-		data: { auth: { requireUserType: UserTypes.ICanUseAdminPanel } }
-	},
-	{
-		path: 'about_me', component: AdminAboutMeComponent,
-		canActivate: [AuthGuard],
-		data: { auth: { requireUserType: UserTypes.ICanUseAdminPanel } }
-	},
-	{ path: 'tenants', loadChildren: '../admin_tenants_module/admin_tenants.module#AdminTenantsModule' },
-	{ path: 'users', loadChildren: '../admin_users_module/admin_users.module#AdminUsersModule' },
-	{ path: 'roles', loadChildren: '../admin_roles_module/admin_roles.module#AdminRolesModule' },
-	{ path: 'settings', loadChildren: '../admin_settings_module/admin_settings.module#AdminSettingsModule' },
-	{ path: 'scheduled_tasks', loadChildren: '../admin_scheduled_tasks_module/admin_scheduled_tasks.module#AdminScheduledTasksModule' },
-	{ path: 'example_datas', loadChildren: '../admin_example_datas_module/admin_example_datas.module#AdminExampleDatasModule' }
+    {
+        path: "",
+        component: AdminContainerComponent,
+        canActivate: [AuthGuard],
+        data: { auth: { requireUserType: UserTypes.ICanUseAdminPanel } },
+        children:
+        [
+            {
+                path: '', component: AdminIndexComponent, pathMatch: "full"
+            },
+            {
+                path: 'about_website', component: AdminAboutWebsiteComponent,
+                canActivate: [AuthGuard],
+                data: { auth: { requireUserType: UserTypes.ICanUseAdminPanel } }
+            },
+            {
+                path: 'about_me', component: AdminAboutMeComponent,
+                canActivate: [AuthGuard],
+                data: { auth: { requireUserType: UserTypes.ICanUseAdminPanel } }
+            },
+            { path: 'tenants', loadChildren: '../admin_tenants_module/admin_tenants.module#AdminTenantsModule' },
+            { path: 'users', loadChildren: '../admin_users_module/admin_users.module#AdminUsersModule' },
+            { path: 'roles', loadChildren: '../admin_roles_module/admin_roles.module#AdminRolesModule' },
+            { path: 'settings', loadChildren: '../admin_settings_module/admin_settings.module#AdminSettingsModule' },
+            { path: 'scheduled_tasks', loadChildren: '../admin_scheduled_tasks_module/admin_scheduled_tasks.module#AdminScheduledTasksModule' },
+            { path: 'example_datas', loadChildren: '../admin_example_datas_module/admin_example_datas.module#AdminExampleDatasModule' }
+        ]
+    },
+    { path: 'login', component: AdminLoginComponent }
+	//{
+	//	path: '',
+	//	component: AdminIndexComponent,
+	//	pathMatch: 'full',
+	//	canActivate: [AuthGuard],
+	//	data: { auth: { requireUserType: UserTypes.ICanUseAdminPanel } }
+	//},
+	//{ path: 'login', component: AdminLoginComponent },
+	//{
+	//	path: 'about_website', component: AdminAboutWebsiteComponent,
+	//	canActivate: [AuthGuard],
+	//	data: { auth: { requireUserType: UserTypes.ICanUseAdminPanel } }
+	//},
+	//{
+	//	path: 'about_me', component: AdminAboutMeComponent,
+	//	canActivate: [AuthGuard],
+	//	data: { auth: { requireUserType: UserTypes.ICanUseAdminPanel } }
+	//},
+	//{ path: 'tenants', loadChildren: '../admin_tenants_module/admin_tenants.module#AdminTenantsModule' },
+	//{ path: 'users', loadChildren: '../admin_users_module/admin_users.module#AdminUsersModule' },
+	//{ path: 'roles', loadChildren: '../admin_roles_module/admin_roles.module#AdminRolesModule' },
+	//{ path: 'settings', loadChildren: '../admin_settings_module/admin_settings.module#AdminSettingsModule' },
+	//{ path: 'scheduled_tasks', loadChildren: '../admin_scheduled_tasks_module/admin_scheduled_tasks.module#AdminScheduledTasksModule' },
+	//{ path: 'example_datas', loadChildren: '../admin_example_datas_module/admin_example_datas.module#AdminExampleDatasModule' }
 ];
 
 @NgModule({
