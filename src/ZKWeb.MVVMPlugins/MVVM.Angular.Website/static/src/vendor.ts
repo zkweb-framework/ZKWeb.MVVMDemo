6c706a406c8e4c87055b96fd75c91cb91d96984a
dudes-come <dudes@qq.com>
2017-05-11 17:30:10 +0800

    优化chunk生成的大小

--- src/ZKWeb.MVVMPlugins/MVVM.Angular.Website/static/src/vendor.ts
+++ src/ZKWeb.MVVMPlugins/MVVM.Angular.Website/static/src/vendor.ts
@@ -1,19 +1,35 @@
 // Angular 2
 import '@angular/platform-browser';
+import '@angular/platform-browser/animations';
 import '@angular/platform-browser-dynamic';
 import '@angular/core';
 import '@angular/common';
 import '@angular/forms';
 import '@angular/http';
 import '@angular/router';
+import '@angular/forms';
 
 // PrimeNG
-// import 'primeng/primeng';
+import 'primeng/components/inputtext/inputtext';
+import 'primeng/components/panel/panel';
+import 'primeng/components/button/button';
+import 'primeng/components/messages/messages';
+import 'primeng/components/blockui/blockui';
+import 'primeng/components/datatable/datatable';
+import 'primeng/components/dropdown/dropdown';
+import 'primeng/components/multiselect/multiselect';
+import 'primeng/components/dialog/dialog';
+import 'primeng/components/confirmdialog/confirmdialog';
+import 'primeng/components/inputtextarea/inputtextarea';
+import 'primeng/components/common/api';
 
 // RxJS
 import 'rxjs/Rx';
+import 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/mergeMap';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/publishLast';
 
 // Styles
 import './vendor/styles/primeng/primeng.min.css';
