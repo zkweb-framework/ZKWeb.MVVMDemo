79227a2d273d025af462c8b84433d187f3271e19
dudes-come <dudes@qq.com>
2017-08-29 14:07:16 +0800

    update publish.bat and publish_ubuntu.sh

--- publish.bat
+++ publish.bat
@@ -1,18 +1,25 @@
 @echo off
+echo this script is for build and publish mvvm demo site
+echo please ensure you have this directory layout
+echo - zkweb
+echo   - tools
+echo - zkweb.demo
+echo   - publish.bat
+echo.
 
 echo building project...
 cd src\ZKWeb.MVVMDemo.AspNetCore
 dotnet restore
-dotnet build -c Release -f netcoreapp1.1
-dotnet publish -c Release -f netcoreapp1.1 -r win10-x64
+dotnet build -c Release -f netcoreapp2.0
+dotnet publish -c Release -f netcoreapp2.0 -r win10-x64
 cd ..\..
 
 echo building plugins...
 cd src\ZKWeb.MVVMDemo.Console
 dotnet restore
-dotnet run -c Release -f netcoreapp1.1
+dotnet run -c Release -f netcoreapp2.0
 cd ..\..
 
 echo publishing website...
-..\ZKWeb\Tools\WebsitePublisher.Cmd.exe -f netcoreapp1.1 -x ".*node_modules.*" -r src\ZKWeb.MVVMDemo.AspNetCore -n "zkweb.mvvm" -o "..\..\publish"
+..\ZKWeb\Tools\WebsitePublisher.Cmd.Windows\ZKWeb.Toolkits.WebsitePublisher.Cmd.exe -f netcoreapp2.0 -x ".*node_modules.*" -r src\ZKWeb.MVVMDemo.AspNetCore -n "zkweb.mvvm" -o "..\..\publish"
 pause
--- publish_ubuntu.sh
+++ publish_ubuntu.sh
@@ -11,7 +11,7 @@ if [ ! -f "/usr/lib/gdiplus.dll" ]; then
     exit
 fi
 
-if [ ! -d "../ZKWeb/ZKWeb.Toolkits" ]; then
+if [ ! -d "../ZKWeb/Tools" ]; then
     echo "ERROR:"
     echo "please download ZKWeb and put it in the same directory as ZKWeb.MVVMDemo"
     exit
@@ -20,17 +20,15 @@ fi
 echo "building project..."
 cd src/ZKWeb.MVVMDemo.AspNetCore
 dotnet restore
-dotnet build -c Release -f netcoreapp1.1
-dotnet publish -c Release -f netcoreapp1.1 -r ubuntu.16.04-x64
+dotnet build -c Release -f netcoreapp2.0
+dotnet publish -c Release -f netcoreapp2.0 -r ubuntu.16.04-x64
 cd ../..
 
 echo "building plugins..."
 cd src/ZKWeb.MVVMDemo.Console
 dotnet restore
-dotnet run -c Release -f netcoreapp1.1
+dotnet run -c Release -f netcoreapp2.0
 cd ../..
 
 echo "publishing website..."
-cd ../ZKWeb/ZKWeb.Toolkits/ZKWeb.Toolkits.WebsitePublisher.Cmd
-dotnet restore
-dotnet run -c Debug -f netcoreapp1.1 -- -f netcoreapp1.1 -x ".*node_modules.*" -r ../../../ZKWeb.MVVMDemo/src/ZKWeb.MVVMDemo.AspNetCore -n "publish" -o "../../../ZKWeb.MVVMDemo"
+dotnet ../ZKWeb/Tools/WebsitePublisher.Cmd.NetCore/ZKWeb.Toolkits.WebsitePublisher.Cmd.dll -f netcoreapp2.0 -x ".*node_modules.*" -r "src/ZKWeb.MVVMDemo.AspNetCore" -n "zkweb.mvvm" -o "../../publish"
